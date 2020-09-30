7a2b50b5e209fdd285710626bc361aa6604383cf
katawohl <katawohl@gmail.com>
2020-09-30 14:38:18 +0200

    added service to send back data with population

--- app/Controllers/covidDataController.js
+++ app/Controllers/covidDataController.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
-const covidDataService = require("../Services/covidDataService");
+const dataFormationService = require("../Services/dataFormationService");
 const cache = require("../Middlewares/memoryCache");
 
 router.get("/summary", cache(10), async (req, res) => {
-  const summary = await fetch("https://api.covid19api.com/summary");
-
+  const countriesCovid = await fetch("https://api.covid19api.com/summary");
+  const countriesPopulation = await fetch(
+    "https://restcountries.eu/rest/v2/all"
+  );
   try {
-    let json = await summary.json();
-    res.status(200).json(json.Countries);
-    covidDataService.covidDataService();
+    const covidDataRaw = await countriesCovid.json();
+    const populationRaw = await countriesPopulation.json();
+
+    const data = await dataFormationService.dataFormationService(
+      covidDataRaw.Countries,
+      populationRaw
+    );
+
+    res.status(200).json(data);
   } catch (error) {
     console.log(`Controller error: ${error}`);
     return res.status(400);
