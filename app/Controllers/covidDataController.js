fd1bdbcfe974d4bf539574177f67e687234d2e63
Áron Esztergomi <aaron.esztergomi@gmail.com>
2020-10-01 19:00:06 +0200

    Created LED controller files and functions.

--- app/Controllers/covidDataController.js
+++ app/Controllers/covidDataController.js
@@ -3,25 +3,26 @@ const router = express.Router();
 const fetch = require("node-fetch");
 const countryDataFormationService = require("../Services/countryDataFormationService");
 const globalDataCalcService = require("../Services/globalDataCalcService");
+const LEDCalcService = require("../Services/LEDCalcService");
 const cache = require("../Middlewares/memoryCache");
 
 router.get("/summary", cache(600), async (req, res) => {
   const countriesCovid = await fetch("https://api.covid19api.com/summary");
   const countriesPopulation = await fetch(
-    "https://restcountries.eu/rest/v2/all"
+      "https://restcountries.eu/rest/v2/all"
   );
   try {
     const covidDataRaw = await countriesCovid.json();
     const populationRaw = await countriesPopulation.json();
 
     const countryData = await countryDataFormationService.countryDataFormationService(
-      covidDataRaw.Countries,
-      populationRaw
+        covidDataRaw.Countries,
+        populationRaw
     );
 
     const globalData = await globalDataCalcService.globalDataCalcService(
-      covidDataRaw.Global,
-      countryData
+        covidDataRaw.Global,
+        countryData
     );
 
     const data = {
@@ -30,6 +31,49 @@ router.get("/summary", cache(600), async (req, res) => {
     };
 
     res.status(200).json(data);
+  } catch (error) {
+    console.log(`Controller error: ${error}`);
+    return res.status(400);
+  }
+});
+
+router.post("/country", async (req, res) => {
+  let recovered=req.body.recovered;
+  let confirmed=req.body.confirmed;
+  let deaths=req.body.deaths;
+
+  if(recovered==null || confirmed==null || deaths==null || isNaN(recovered) ||
+      isNaN(confirmed) || isNaN(deaths) || !Number.isInteger(recovered) ||
+      !Number.isInteger(confirmed) || !Number.isInteger(deaths)){
+    return res.status(400).json({"error": "Some of the input fields are invalid!"});
+  }
+
+  try {
+    const data = await LEDCalcService.LEDCalcService(
+        recovered,
+        confirmed,
+        deaths
+    );
+
+    let response = await fetch("http://backend.bassboost.hu/hackathon/send/" +
+        "711fdedf-a013-41de-9b07-b30314e3258c", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({configText: data})
+    });
+
+    setTimeout( async function(){
+      await fetch("http://backend.bassboost.hu/hackathon/stop/" +
+         "711fdedf-a013-41de-9b07-b30314e3258c", {
+       method: 'POST'
+      });
+    },10000);
+
+    setTimeout( function(){
+      res.status(response.status).json({message: response.statusText});
+    },15000);
+
   } catch (error) {
     console.log(`Controller error: ${error}`);
     return res.status(400);
